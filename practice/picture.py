2faa44a4e1de73722ca42611f77d0db65584aefe
Moonlight-CHENG <84613252+Moonlight-CHENG@users.noreply.github.com>
2023-08-01 21:47:21 +0800

    Add files via upload

---
+++ practice/picture.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+import torch
+
+img = cv2.imread('1.jpg')
+cv2.imshow('img', img)
+
+num = np.zeros(img.shape, img.dtype) + 150
+imgIncrease = cv2.add(img, num)
+imgSubtract = cv2.subtract(img, num)
+
+# cv2.imwrite("2.jpg",imgIncrease)
+# cv2.imwrite("3.jpg",imgSubtract)
+cv2.imshow('img', img)
+cv2.imshow('increase', imgIncrease)
+cv2.imshow('subtract', imgSubtract)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
